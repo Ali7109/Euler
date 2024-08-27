4165472e8ff8e9be01ff75738cee906b9e0a84ae
Ali Amin <aliamin745@gmail.com>
2024-08-26 21:04:12 -0400

    Created file parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include "parser.h"
 #include <iomanip>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::string code = "6+pow(8, 0)*8+2";
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <script.eulr>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file)
+    {
+        std::cerr << "Error: Could not open file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string code = buffer.str();
 
     Lexer lexer(code);
     std::vector<Token> tokens = lexer.tokenize();
