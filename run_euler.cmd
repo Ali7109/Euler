b4d389211559acf660a84e9ba930b2964232663c
Ali Amin <aliamin745@gmail.com>
2024-08-29 21:36:36 -0400

    Created more test files, added more extension support

--- run_euler.cmd
+++ run_euler.cmd
@@ -12,11 +12,21 @@ if not "%~2"=="" (
 )
 
 :: Check if the argument has a .eulr extension
-if /i "%~x1"==".eulr" (
+if /i "%~x1"==".eulr" GOTO RUNEULR
+if /i "%~x1"==".elr" GOTO RUNEULR
+if /i "%~x1"==".eu" GOTO RUNEULR
+
+echo Invalid file extension. Please provide a .eulr file.
+exit /b 1
+
+:RUNEULR
+if EXIST "%~1" (
+    echo:
     CALL .\build\bin\Debug\EulerLang.exe %1
-    echo :
-    echo "Check execution.log for more details..."
-) else (
-    echo Invalid file extension. Please provide a .eulr file.
+    echo:
+    echo "Euler ran. Check execution.log for more details..."
+    exit /b 0
+) ELSE (
+    echo Error: File not found
     exit /b 1
 )
